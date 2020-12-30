fede901e4aec13b1da47f03c0ab1176e8d37ba9a
Mendes <foes@gft.com>
2020-12-30 14:04:27 -0300

    Repositorio Padrão

---
+++ Marraia.MongoDb/Repositories/MongoDbRepositoryStandard.cs
@@ -0,0 +1,23 @@
+﻿using Marraia.MongoDb.Core;
+using Marraia.MongoDb.Repositories.Interfaces;
+using MongoDB.Driver;
+
+namespace Marraia.MongoDb.Repositories
+{
+    public class MongoDbRepositoryStandard<TEntity, TKey> 
+                   where TEntity : Entity<TKey>
+                   where TKey : struct
+    {
+        private readonly IMongoContext _context;
+
+        protected IMongoCollection<TEntity> Collection
+        {
+            get
+            {
+                return _context
+                        .Database
+                        .GetCollection<TEntity>(typeof(TEntity).Name);
+            }
+        }
+    }
+}
