0cf099f232f28ff3a745c969c0ea884b587e0939
Fernando Mendes <fernando.mendes@myprofitweb.com>
2024-12-11 21:40:27 -0300

    [Update] - Alterações sobre serialização de GUID no mongo

--- Marraia.MongoDb/obj/Debug/netstandard2.1/Marraia.MongoDb.AssemblyInfo.cs
+++ Marraia.MongoDb/obj/Debug/netstandard2.1/Marraia.MongoDb.AssemblyInfo.cs
@@ -15,11 +15,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Pronto para usar, facilite sua conexão com o MongoDb. Métodos de Insert, Update, " +
     "Delete e Select já prontos para utilizar em seus projetos.")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.1.4.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.4+aee7f2fa24c2e26d02a49f471a115df832a19c60")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.1.5.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.5+cf848a842110d50f31f57434dd7861b87a1f437f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Marraia.MongoDb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Marraia.MongoDb")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.1.4.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.1.5.0")]
 [assembly: System.Reflection.AssemblyMetadataAttribute("RepositoryUrl", "https://github.com/marraia/marraia.mongoDb")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
